4ce55c644bcb2ddc63893d1533354b48e9d3adc1
Kean Freeman <keanfree@umich.edu>
2023-03-10 10:55:04 -0800

    changed pathfinder to use a priority queue to speed up searches

--- Assets/Scripts/Pathfinding/Pathfinder.cs
+++ Assets/Scripts/Pathfinding/Pathfinder.cs
@@ -9,10 +9,12 @@ public class Pathfinder
     private VoxelPlayEnvironment environment;
     private SpriteMovement spriteMovement;
 
+    private const int MAX_PATH_LENGTH = 1000;
 
-    PriorityQueue<Node, int> priorityQueue = new PriorityQueue<Node, int>();
-    List<Node> frontier = new List<Node>();
+    PriorityQueue<Node, float> frontier = new PriorityQueue<Node, float>();
     Dictionary<Vector3Int, Node> positionToNode = new Dictionary<Vector3Int, Node>();
+    // keeps track of changed values since we can't update them directly in the priority queue
+    private Dictionary<Node, float> changedNodes = new Dictionary<Node, float>();
     List<Vector3Int> path = new List<Vector3Int>();
 
     public Pathfinder(VoxelPlayEnvironment environment, SpriteMovement spriteMovement) {
@@ -31,18 +33,18 @@ public class Pathfinder
         start.score = 0;
         start.heuristicScore = 0;
 
-        frontier.Add(start);
-        frontier.Add(end);
-        positionToNode[end.position] = end;
+        frontier.Enqueue(start, start.heuristicScore);
+        positionToNode[start.position] = start;
 
         Node currNode;
         while (true) {
             if (frontier.Count > 10000) {
+                Debug.Log("Ran into infinite path");
                 return path;
             }
 
             currNode = GetLowestHeuristicScoreUnvisited();
-            if (currNode == null || currNode.score >= 1000) {
+            if (currNode == null || currNode.score >= MAX_PATH_LENGTH) {
                 Debug.Log("Path would be too long, stopping search.");
                 return path;
             }
@@ -50,28 +52,33 @@ public class Pathfinder
 
             // get neighbors of current node
             foreach (Vector3Int coordinate in Coordinates.GetAdjacentCoordinates(currNode.position)) {
-                Node neighborNode;
+                Node neighbor;
                 if (!positionToNode.ContainsKey(coordinate)) {
-                    positionToNode[coordinate] = new Node(coordinate);
-                    frontier.Add(positionToNode[coordinate]);
+                    neighbor = new Node(coordinate);
+                    positionToNode[coordinate] = neighbor;
                 }
-                neighborNode = positionToNode[coordinate];
-                currNode.neighbors.Add(neighborNode);
-            }
+                neighbor = positionToNode[coordinate];
+                currNode.neighbors.Add(neighbor);
 
-            // update neighbor costs
-            foreach (Node neighbor in currNode.neighbors) {
+                // update neighbor costs
                 if (!neighbor.visited) {
                     float distanceToNeighbor = CalculateScore(currNode, neighbor);
                     if (distanceToNeighbor < neighbor.score) {
                         neighbor.score = distanceToNeighbor;
-                        neighbor.heuristicScore = neighbor.score 
-                            + CalculateHeuristicScore(neighbor, end);
+                        neighbor.heuristicScore = neighbor.score + CalculateHeuristicScore(neighbor, end);
                         neighbor.prevNode = currNode;
+
+                        // priority has changed
+                        if (!changedNodes.ContainsKey(neighbor)) {
+                            changedNodes[neighbor] = float.MaxValue;
+                        }
+                        changedNodes[neighbor] = neighbor.heuristicScore;
                     }
+                    frontier.Enqueue(neighbor, neighbor.heuristicScore);
                 }
             }
-            if (currNode == end) {
+
+            if (currNode.position == end.position) {
                 Debug.Log("Finished path");
                 while (true) {
                     path.Add(currNode.position);
@@ -96,14 +103,25 @@ public class Pathfinder
     }
 
     private Node GetLowestHeuristicScoreUnvisited() {
-        float lowestScore = float.MaxValue;
-        Node lowestNode = null;
-        foreach (Node node in frontier) {
-            if (node.heuristicScore < lowestScore && !node.visited) {
-                lowestScore = node.heuristicScore;
-                lowestNode = node;
+        while (true) {
+            if (frontier.Count == 0) {
+                return null;
             }
+
+            // get rid of visited or outdated nodes
+            Node topNode = frontier.Peek();
+            if (topNode.visited) {
+                frontier.Dequeue();
+                continue;
+            }
+            if (changedNodes.ContainsKey(topNode) && changedNodes[topNode] < topNode.heuristicScore) {
+                frontier.Dequeue();
+                frontier.Enqueue(topNode, changedNodes[topNode]);
+                continue;
+            }
+            break;
         }
-        return lowestNode;
+
+        return frontier.Dequeue();
     }
 }
